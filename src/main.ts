9b769fae33862299087187228733773cb7e3c4f6
Nickolay Shishelyakin <shne2003@gmail.com>
2024-02-19 16:52:05 +0300

    add static

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
-  await app.listen(process.env.PORT ?? 3000);
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'WEB_Labs'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+  const port = process.env.PORT ?? 3000;
+  console.log(port);
+  await app.listen(port);
 }
 bootstrap();
